a2a60da9b1fa426d1d76bf1d1e10df50401edae3
almat101 <sandruozzo@gmail.com>
2026-03-30 19:05:51 +0200

    feat(frontend): enable reverse proxy with Nginx and update API configuration
    - Added Nginx upstream and reverse proxy configuration for backend service on /api/ path.
    - Updated Angular ApiConfiguration to use relative rootUrl (/api/v1).
    - Now the frontend communicates with the backend securely via Nginx reverse proxy on port 443, without exposing backend ports externally.

--- frontend/ANGULAR/src/app/services/api-configuration.ts
+++ frontend/ANGULAR/src/app/services/api-configuration.ts
@@ -9,7 +9,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8088/api/v1';
+  // rootUrl: string = 'http://localhost:8088/api/v1';
+  rootUrl: string = '/api/v1';
+
 }
 
 /**
